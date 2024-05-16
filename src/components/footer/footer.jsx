5754838a2f65fda3c16598b6b888a89f41e71d23
Tatyana-Shevchuk <130395557+Tatyana-Shevchuk@users.noreply.github.com>
2024-05-16 17:42:39 +0300

    Add files via upload

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css';
+
+function Footer({ tasks }) {
+
+  return (
+    <footer className='footer'>
+      <p className='footer__task-count'>Active tasks: {tasks.filter(task => task.status === "backlog").length}</p>
+      <p className='footer__task-count'>Finished tasks: {tasks.filter(task => task.status === "finished").length}</p>
+      <p className='footer__kanban-credits'>Kanban board by fxfc, 2024</p>
+    </footer>
+  )
+}
+
+export default Footer;
